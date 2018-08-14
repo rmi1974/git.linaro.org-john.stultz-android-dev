jcadduono/android_kernel_google_chromeos

ANDROID: vfs: add d_canonical_path for stacked filesystem support
Inotify does not currently know when a filesystem
is acting as a wrapper around another fs. This means
that inotify watchers will miss any modifications to
the base file, as well as any made in a separate
stacked fs that points to the same file.
d_canonical_path solves this problem by allowing the fs
to map a dentry to a path in the lower fs. Inotify
can use it to find the appropriate place to watch to
be informed of all changes to a file.

Signed-off-by: Daniel Rosenberg <drosen@google.com>
(cherry picked from commit 96fe5c2548e745d2b2a95dd5cf800d4e36f81d26)

BUG=b:27794037
TEST=Build and boot on Smaug.

Change-Id: I063ea9c687df919c8e56046584ca9ebcd68c2a00
Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/335184
Reviewed-by: Daniel Rosenberg <drosen@google.com>

include/linux/dcache.h
@@ -160,6 +160,7 @@ struct dentry_operations {
 	char *(*d_dname)(struct dentry *, char *, int);
 	struct vfsmount *(*d_automount)(struct path *);
 	int (*d_manage)(struct dentry *, bool);
+	void (*d_canonical_path)(const struct dentry *, struct path *);
 } ____cacheline_aligned;
 
 /*

rmi1974/git.linaro.org-john.stultz-android-dev

Change-Id: I09563baffad1711a045e45c1bd0bd8713c2cc0b6

include/linux/dcache.h
@@ -147,6 +147,7 @@ struct dentry_operations {
 	int (*d_manage)(const struct path *, bool);
 	struct dentry *(*d_real)(struct dentry *, const struct inode *,
 				 unsigned int, unsigned int);
+	void (*d_canonical_path)(const struct path *, struct path *);
 } ____cacheline_aligned;
 
 /*