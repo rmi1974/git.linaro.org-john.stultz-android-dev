a1043b7adba86e34e99c3c80389dce028368bcad
John Stultz <john.stultz@linaro.org>
2019-01-30 13:34:56 -0800

    HACK: cpuidle: menu govenor hack from VincentD
    VincentD suggested the following hack to improve
    graphics performance
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    
    Change-Id: I099c89d77b8b518f52f300d6670be47d9d3d806e

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -490,6 +490,7 @@ static void menu_update(struct cpuidle_driver *drv, struct cpuidle_device *dev)
 	 * assume the state was never reached and the exit latency is 0.
 	 */
 
+#if 0
 	if (data->tick_wakeup && data->next_timer_us > TICK_USEC) {
 		/*
 		 * The nohz code said that there wouldn't be any events within
@@ -501,7 +502,9 @@ static void menu_update(struct cpuidle_driver *drv, struct cpuidle_device *dev)
 		 * duration predictor do a better job next time.
 		 */
 		measured_us = 9 * MAX_INTERESTING / 10;
-	} else if ((drv->states[last_idx].flags & CPUIDLE_FLAG_POLLING) &&
+	} else
+#endif
+	if ((drv->states[last_idx].flags & CPUIDLE_FLAG_POLLING) &&
 		   dev->poll_time_limit) {
 		/*
 		 * The CPU exited the "polling" state due to a time limit, so
