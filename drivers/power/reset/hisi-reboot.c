b6df5b164ecc20f96aaf491284e96ed7e2911809
John Stultz <john.stultz@linaro.org>
2019-08-17 04:26:31 +0000

    HACK: Allow hisi-reboot to be a module
    Configure the  hisi-reboot driver to be a module.
    
    This requires some ugly hacks which I need to sort out.
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Change-Id: I5d9ccf4de45e9482ae93b3d700dc46e2a6a84853

--- drivers/power/reset/hisi-reboot.c
+++ drivers/power/reset/hisi-reboot.c
@@ -68,7 +68,7 @@ static int hisi_restart_handler(struct notifier_block *this,
 	}
 
 	while (1)
-		cpu_do_idle();
+		mdelay(1);
 
 	return NOTIFY_DONE;
 }
@@ -111,7 +111,7 @@ static int hisi_reboot_probe(struct platform_device *pdev)
 		iounmap(base);
 	}
 
-	arm_pm_restart = NULL;
+//	arm_pm_restart = NULL;
 	return err;
 }
 
@@ -129,3 +129,6 @@ static struct platform_driver hisi_reboot_driver = {
 	},
 };
 module_platform_driver(hisi_reboot_driver);
+
+MODULE_DESCRIPTION("Reset driver for HiSi SoCs");
+MODULE_LICENSE("GPL v2");
