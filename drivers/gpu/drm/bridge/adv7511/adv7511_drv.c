f3ac089b7320334e347b2f30a392c71e953255c1
John Stultz <john.stultz@linaro.org>
2019-12-07 05:01:33 +0000

    HACK: adv7511: Drop hotplug event to avoid initialization races
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Change-Id: Idd15ab7f4a9bde2d3d53f171312a2bce2a7e000e

--- drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
+++ drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
@@ -448,7 +448,7 @@ static void adv7511_hpd_work(struct work_struct *work)
 		adv7511->connector.status = status;
 		if (status == connector_status_disconnected)
 			cec_phys_addr_invalidate(adv7511->cec_adap);
-		drm_kms_helper_hotplug_event(adv7511->connector.dev);
+//		drm_kms_helper_hotplug_event(adv7511->connector.dev);
 	}
 }
 
