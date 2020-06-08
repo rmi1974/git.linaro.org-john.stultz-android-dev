abdc869bcccc94b46fc6a64b43000f42782593cc
John Stultz <john.stultz@linaro.org>
2020-06-08 23:36:31 +0000

    workaround for smmu crash
    Change-Id: I36c6e88e42d97be3986e7bcd7b29e093713fcaa5

--- drivers/iommu/arm-smmu-qcom.c
+++ drivers/iommu/arm-smmu-qcom.c
@@ -19,7 +19,7 @@ static const struct of_device_id qcom_smmu_client_of_match[] = {
 	{ .compatible = "qcom,mdss" },
 	{ .compatible = "qcom,sc7180-mdss" },
 	{ .compatible = "qcom,sc7180-mss-pil" },
-	{ .compatible = "qcom,sdm845-mdss" },
+//	{ .compatible = "qcom,sdm845-mdss" },
 	{ .compatible = "qcom,sdm845-mss-pil" },
 	{ }
 };
