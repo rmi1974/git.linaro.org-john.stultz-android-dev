c6731ccf352cd54c1e3b9ed4e0ca87560915747c
John Stultz <john.stultz@linaro.org>
2020-06-09 21:43:47 +0000

    vma naming fixup for 5.8-rc1
    Change-Id: I41c1a5cf0674be1ff26b80938c78283cc32e05f9

--- kernel/sys.c
+++ kernel/sys.c
@@ -2402,7 +2402,7 @@ static int prctl_set_vma(unsigned long opt, unsigned long start,
 	if (end == start)
 		return 0;
 
-	down_write(&mm->mmap_sem);
+	mmap_write_lock(mm);
 
 	switch (opt) {
 	case PR_SET_VMA_ANON_NAME:
@@ -2412,7 +2412,7 @@ static int prctl_set_vma(unsigned long opt, unsigned long start,
 		error = -EINVAL;
 	}
 
-	up_write(&mm->mmap_sem);
+	mmap_write_unlock(mm);
 
 	return error;
 }
